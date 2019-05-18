a2eeee37b0282f12444c269e39914513edeb01c7
AlexisAndre34 <alexis.andrepro34@gmail.com>
2019-05-18 20:50:58 +0200

    config gitignore

--- projectWeb/settings.py
+++ projectWeb/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'app',
+    'storages',
 ]
 
 CRISPY_TEMPLATE_PACK ='bootstrap4'
@@ -137,3 +138,14 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
+
+
+#django storages documentation to learn more 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY =  os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False #prevent overwriting file with the same name when uploaded
+AWS_DEFAULT_ACL = None 
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
